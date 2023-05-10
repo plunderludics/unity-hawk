217aa4ffd7d414faf1a31a9e3df8f6f64855a0d3
hello <hello>
2023-05-10 11:20:51 -0400

    first pass at controls, + broken attempt at audio

--- Assets/TestBizHawk.cs
+++ Assets/TestBizHawk.cs
@@ -11,17 +11,33 @@ using System.IO;
 public class TestBizHawk : MonoBehaviour
 {
     IEmulator emulator;
-    IController controller;
     IVideoProvider videoProvider;
+    ISoundProvider soundProvider;
+
+    InputManager inputManager;
+    UnityInputProvider inputProvider;
+
+    IDialogParent dialogParent;
+
+    MovieSession movieSession; // annoying that we need this at all
 
     RomLoader loader;
     Config config;
     FirmwareManager firmwareManager = new FirmwareManager();
 
     string bizhawkDir = Path.Combine(Application.dataPath, "BizHawk");
+    string romsDir = Path.Combine(Application.dataPath, "Roms");
+
+    public string romFileName = "mario.nes";
 
     public Renderer targetRenderer;
     Texture2D targetTexture;
+
+    static int RunningAudioBufferSize = 4096;
+    short[] runningAudioBuffer;
+    int runningAudioBufferLength;
+
+    public int frame = 0;
 
     void Awake()
     {
@@ -29,17 +45,49 @@ public class TestBizHawk : MonoBehaviour
         // Note: the gamedb directory MUST already have the gamedb.txt file, otherwise seems to hang :(
         string gamedbPath = Path.Combine(bizhawkDir, "gamedb");
         Database.InitializeDatabase(
-            bundledRoot:gamedbPath,
+            bundledRoot: gamedbPath,
             userRoot: gamedbPath,
             silent: true);
         BootGodDb.Initialize(gamedbPath);
         MAMEMachineDB.Initialize(gamedbPath);
+
+        // Check if there is an AudioSource attached
+        if (!GetComponent<AudioSource>()) {
+            Debug.LogWarning("No AudioSource component, will not play emulator audio");
+        }
+
+        runningAudioBuffer = new short[RunningAudioBufferSize];
+        runningAudioBufferLength = 0;
+
+        inputManager = new InputManager();
+        inputProvider = new UnityInputProvider();
+        
+        dialogParent = new UnityDialogParent();
     }
+
     void Start()
     {
+        // Load config
         var configPath = Path.Combine(bizhawkDir, "config.ini");
         Debug.Log(configPath);
         config = ConfigService.Load<Config>(configPath);
+
+        // Init controls
+        inputManager.ControllerInputCoalescer = new(); // seems to be necessary
+
+        // Set up movie session
+        // [this is kind of stupid because we don't really need a movie session
+        //  but InputManager expects a MovieSession as part of the input chain :/
+        //  i guess the alternative would be to reimplement InputManager]
+        movieSession = new MovieSession(
+				config.Movies,
+				config.PathEntries.MovieBackupsAbsolutePath(),
+				dialogParent,
+				null,
+				() => {},
+				() => {}
+        );
+
         loader = new RomLoader(config) {
             //ChoosePlatform = ChoosePlatformForRom,
         };
@@ -48,7 +96,7 @@ public class TestBizHawk : MonoBehaviour
         loader.OnLoadSettings += CoreSettings;
         loader.OnLoadSyncSettings += CoreSyncSettings;
 
-        var romPath = Path.Combine(Application.dataPath, "mario.nes");
+        var romPath = Path.Combine(romsDir, romFileName);
         var nextComm = CreateCoreComm();
 
         bool loaded = loader.LoadRom(romPath, nextComm, null);
@@ -56,39 +104,111 @@ public class TestBizHawk : MonoBehaviour
         if (loaded) {
             Debug.Log($"Loaded {romPath}!");
 
-            controller = new NullController();
             emulator = loader.LoadedEmulator;
 
-            videoProvider = emulator.AsVideoProvider();
+            videoProvider = emulator.AsVideoProviderOrDefault();
+            soundProvider = emulator.AsSoundProviderOrDefault();
 
             Debug.Log($"{videoProvider.BufferWidth} x {videoProvider.BufferHeight}");
             targetTexture = new Texture2D(videoProvider.BufferWidth, videoProvider.BufferHeight);
             targetRenderer.material.mainTexture = targetTexture;
 
+            // [not sure what this does but seems important]
+		    inputManager.SyncControls(emulator, movieSession, config);
         } else {
             Debug.LogWarning($"Failed to load {romPath}.");
         }
     }
 
+    // [Not really sure what the framerate of this should be tbh - should check what BizHawk does]
     void Update()
     {
         if (emulator != null) {
-            emulator.FrameAdvance(controller, true, true);
-            // Debug.Log(emulator.Frame);
+            // Input handling
+            inputProvider.Update(); // (this should read in all the Unity input and store it in a queue)
+            var finalHostController = inputManager.ControllerInputCoalescer;
+            // InputManager.ActiveController.PrepareHapticsForHost(finalHostController);
+            ProcessInput(finalHostController, inputProvider);
+			inputManager.ActiveController.LatchFromPhysical(finalHostController); //[idk what this does]
+            // [there's a bunch more input chain processing stuff in MainForm.cs that we are leaving out for now]
 
-            int[] buffer = videoProvider.GetVideoBuffer();
-            // i guess this just happens to be the right pixel format already
-            targetTexture.SetPixelData(buffer, 0);
+            // [For later:]
+            // if (Tools.Has<LuaConsole>())
+            // {
+            //     Tools.LuaConsole.ResumeScripts(false);
+            // }
+
+            // [gotta call this to make sure the input gets through]
+			movieSession.HandleFrameBefore();
+
+            // Emulator step forward
+            emulator.FrameAdvance(inputManager.ControllerOutput, true, true);
+
+            // [maybe not needed]
+			movieSession.HandleFrameBefore();
+
+            // copy the texture from the emulator to the target renderer
+            // [any faster way to do this?]
+            int[] videoBuffer = videoProvider.GetVideoBuffer();
+            // [pixel format probably doesn't match, but close enough for now lol]
+            targetTexture.SetPixelData(videoBuffer, 0);
             targetTexture.Apply();
+
+
+            // get audio samples for the emulated frame
+            short[] lastFrameAudioBuffer; 
+            int nSamples;
+            soundProvider.GetSamplesSync(out lastFrameAudioBuffer, out nSamples);
+            // Debug.Log($"Got {nSamples} samples this frame.");
+            // [Seems to be 734 samples each frame for mario.nes]
+            // append them to running buffer
+            for (int i = 0; i < nSamples; i++) {
+                runningAudioBuffer[runningAudioBufferLength] = lastFrameAudioBuffer[i];
+                runningAudioBufferLength++;
+            }
+
+
+            frame++;
+        }
+    }
+
+    // [Heads up, will only get called if there is an AudioSource component attached]
+    // [also doesn't work at all right now, idk why]
+    void OnAudioFilterRead(float[] data, int channels) {
+        // Ignore input (contents of `data`), just overwrite with emulator audio
+
+        // Debug.Log($"n channels: {channels}");
+        // Debug.Log($"Unity buffer size: {data.Length}; Emulated audio buffer size: {runningAudioBufferLength}");
+
+        // i have no idea how to deal with framerate mismatch between unity and the emulator right now
+        // for now, just stretch out the audio
+        for (int i = 0; i < data.Length; i++) {
+            short sample = runningAudioBuffer[i*(runningAudioBufferLength/data.Length)];
+            data[i] = sample/32767; // is this even right? probably a better way to do this
+        }
+
+        // consume all accumulated samples (play them) and restart the buffer
+        runningAudioBufferLength = 0;
+    }
+
+    // Based on MainForm:ProcessInput, but with a lot of stuff missing
+    void ProcessInput(
+        ControllerInputCoalescer finalHostController,
+        IInputProvider inputProvider // [not in BizHawk, this is our abstraction of BizHawk's Input class]
+    ) {
+        // loop through all available events
+        InputEvent ie;
+        while ((ie = inputProvider.DequeueEvent()) != null)
+        {
+            // Debug.Log(ie);
+            finalHostController.Receive(ie);
         }
     }
 
     //
     // The rest of the methods are copied / closely adapted from ones in MainForm.cs
     //
-
     CoreComm CreateCoreComm() {
-        var dialogParent = new UnityDialogParent();
         var cfp = new CoreFileProvider(
             dialogParent,
             firmwareManager,
