507987d6e87dca052ee233199b805777c82fdf32
hello <hello>
2023-07-01 06:26:01 -0400

    attempt at a custom lua lib but it won't be so easy sadly

---
+++ Assets/UnityHawk/LuaTestLibrary.cs
@@ -0,0 +1,26 @@
+// [This doesn't work, was too hopeful]
+// [because LuaLibraries does some reflection stuff to load all the subclasses of LuaLibraryBase
+//  but only from within the BizHawk.Client.Common assembly - would need to modify that code to include custom libraries too i guess]
+
+using NLua;
+using BizHawk.Client.Common;
+
+using UnityEngine;
+using System;
+
+public class LuaTestLibrary : LuaLibraryBase {
+    public LuaTestLibrary(ILuaLibraries luaLibsImpl, ApiContainer apiContainer, Action<string> logOutputCallback)
+    : base(luaLibsImpl, apiContainer, logOutputCallback) {}
+
+    public override string Name => "unityhawktest";
+
+    [LuaMethodExample("unityhawktest.sayhello( );")]
+    [LuaMethod("gettestvalue", "says hello")]
+    public void SayHello() {
+        Debug.Log("hello unity from lua");
+    }
+
+    [LuaMethodExample("local xxx = unityhawktest.gettestvalue( );")]
+    [LuaMethod("gettestvalue", "returns 123")]
+    public int GetTestValue() => 123;
+}
