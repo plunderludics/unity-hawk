66e6a8981335750ab933280f2e8e811bfc0f0a05
hello <hello>
2023-07-01 06:46:41 -0400

    oops forgot to add lua test script

---
+++ Assets/test.lua
@@ -0,0 +1,19 @@
+
+function main()
+    print("running main");
+    while true do
+        print("running a frame");
+        emu.yield() -- frameadvance() also works
+        -- console.clear();
+        -- print("test lua");
+        -- client.togglepause();
+
+        -- unityhawktest.sayhello();
+
+        pressed = joypad.getimmediate();
+        print(pressed);
+        -- joypad.set({['P1 C Up'] = true}) -- hold down the first-person look button
+    end
+end
+
+main()
