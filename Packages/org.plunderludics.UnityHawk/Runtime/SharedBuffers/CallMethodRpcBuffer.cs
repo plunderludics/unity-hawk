2560e53beaa5f5bae480fa62456e1fe9167e4e63
iatenothingbutriceforthreedays <darwinvickers@gmail.com>
2024-04-09 00:24:54 +0800

    Show exceptions thrown in code called from bizhawk lua

--- Packages/org.plunderludics.UnityHawk/Runtime/SharedBuffers/CallMethodRpcBuffer.cs
+++ Packages/org.plunderludics.UnityHawk/Runtime/SharedBuffers/CallMethodRpcBuffer.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System;
 using SharedMemory;
 using BizHawk.UnityHawk;
+using System.Runtime.InteropServices.WindowsRuntime;
 public class CallMethodRpcBuffer : ISharedBuffer {
     private string _name;
     private RpcBuffer _rpcBuffer;
@@ -15,35 +16,41 @@ public class CallMethodRpcBuffer : ISharedBuffer {
         _rpcBuffer = new (
             name: _name,
             (msgId, payload) => {
-                // Debug.Log($"callmethod rpc request {string.Join(", ", payload)}");
+                string returnString;
+                try { // Because this runs outside the main threads, we have to catch all exceptions to force them to display in the console
+                    // Debug.Log($"callmethod rpc request {string.Join(", ", payload)}");
 
-                // deserialize payload into method name and args (separated by 0)
-                // [could/should use MethodCall struct here + generic deserialization instead]
-                int methodNameLength = System.Array.IndexOf(payload, (byte)0);
-                byte[] methodNameBytes = new byte[methodNameLength];
-                byte[] argBytes = new byte[payload.Length - methodNameLength - 1];
-                Array.Copy(
-                    sourceArray: payload,
-                    sourceIndex: 0,
-                    destinationArray: methodNameBytes,
-                    destinationIndex: 0,
-                    length: methodNameLength);
-                Array.Copy(
-                    sourceArray: payload,
-                    sourceIndex: methodNameLength + 1,
-                    destinationArray: argBytes,
-                    destinationIndex: 0,
-                    length: argBytes.Length);
+                    // deserialize payload into method name and args (separated by 0)
+                    // [could/should use MethodCall struct here + generic deserialization instead]
+                    int methodNameLength = System.Array.IndexOf(payload, (byte)0);
+                    byte[] methodNameBytes = new byte[methodNameLength];
+                    byte[] argBytes = new byte[payload.Length - methodNameLength - 1];
+                    Array.Copy(
+                        sourceArray: payload,
+                        sourceIndex: 0,
+                        destinationArray: methodNameBytes,
+                        destinationIndex: 0,
+                        length: methodNameLength);
+                    Array.Copy(
+                        sourceArray: payload,
+                        sourceIndex: methodNameLength + 1,
+                        destinationArray: argBytes,
+                        destinationIndex: 0,
+                        length: argBytes.Length);
 
-                string methodName = System.Text.Encoding.ASCII.GetString(methodNameBytes);
-                string argString = System.Text.Encoding.ASCII.GetString(argBytes);
+                    string methodName = System.Text.Encoding.ASCII.GetString(methodNameBytes);
+                    string argString = System.Text.Encoding.ASCII.GetString(argBytes);
 
-                string returnString = _callRegisteredMethod(methodName, argString);
+                    returnString = _callRegisteredMethod(methodName, argString);
 
-                // [messy hack] don't allow returning null because it seems to break things on the other side of the RPC
-                if (returnString == null) {
-                    Debug.LogWarning($"{methodName} returned null but null return values are not supported, converting to empty string");
-                    returnString = "";
+                    // [messy hack] don't allow returning null because it seems to break things on the other side of the RPC
+                    if (returnString == null) {
+                        Debug.LogWarning($"{methodName} returned null but null return values are not supported, converting to empty string");
+                        returnString = "";
+                    }
+                } catch (Exception e) {
+                    Debug.LogException(e);
+                    returnString = ""; // return an empty string to avoid crashing bizhawk
                 }
 
                 byte[] returnData = System.Text.Encoding.ASCII.GetBytes(returnString);
