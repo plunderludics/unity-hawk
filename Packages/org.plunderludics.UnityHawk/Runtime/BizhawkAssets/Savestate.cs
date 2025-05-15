f2aab210e208a24ac12a53c12fdebc795915f819
iatenothingbutriceforthreedays <darwinvickers@gmail.com>
2025-05-16 02:52:07 +0800

    first pass implementation of filtered picker, some issues

--- Packages/org.plunderludics.UnityHawk/Runtime/BizhawkAssets/Savestate.cs
+++ Packages/org.plunderludics.UnityHawk/Runtime/BizhawkAssets/Savestate.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 using UnityEngine.Serialization;
 
 namespace UnityHawk {
+    public class Savestate : BizhawkAsset
+    {
+        [FormerlySerializedAs("GameInfo")]
+        [Header("Game")]
+        public RomInfo RomInfo;
 
-public class Savestate : BizhawkAsset {
-    [FormerlySerializedAs("GameInfo")] [Header("Game")]
-    public RomInfo RomInfo;
+        // TODO: get the rom associated with this game using gamedb
+        public Rom Rom;
 
-    // TODO: get the rom associated with this game using gamedb
-    public Rom Rom;
-
-    public string Name {
-        get => RomInfo.Name != "NULL" ? RomInfo.Name : name;
+    // DV 2025-05-16: Why is this returning the name of the rom? Unused and confusing so commenting it out for now
+        // public string Name {
+        //     get => RomInfo.Name != "NULL" ? RomInfo.Name : name;
+        // }
     }
-}
 }
