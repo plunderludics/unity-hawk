89867046d5f03ed3b8712997b2f728aa19888e55
mut <gustavo.ceci95@gmail.com>
2025-06-26 17:57:03 -0400

    moved demo stuff back to demo folder

--- Packages/org.plunderludics.UnityHawk/Runtime/Emulator/Emulator.cs
+++ Packages/org.plunderludics.UnityHawk/Runtime/Emulator/Emulator.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using BizHawk.Client.Common;
 using JetBrains.Annotations;
