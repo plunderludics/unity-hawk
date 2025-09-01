874dd0966d8e58cfc1920c5d0c775b9c5f65e19a
iatenothingbutriceforthreedays <darwinvickers@gmail.com>
2025-09-01 23:20:35 +0800

    expand Controls to allow mapping from legacy axis inputs and InputActionReferences, and mapping to analog bizhawk-side inputs

--- Packages/org.plunderludics.UnityHawk/Runtime/Input/Controls.cs
+++ Packages/org.plunderludics.UnityHawk/Runtime/Input/Controls.cs
@@ -11,12 +11,37 @@ namespace UnityHawk {
 
 [System.Serializable]
 public class Controls {
+    public enum InputSourceType {
+        KeyCode,                // Keyboard code - should work with either new or legacy input system
+        LegacyAxis,             // Legacy input system axis (Unity input manager)
+        InputActionReference    // New input system action reference
+    }
+
     [System.Serializable]
     public struct KeyCode2Control {
         public bool Enabled;
-        public KeyCode Key; // Keyboard key name e.g. "Z"
-        public string Control; // Bizhawk control name e.g. "D-Pad Up" (Don't include "P1 " prefix!)
-        public Controller Controller; // Which controller this key is for (can be None)
+        public InputSourceType sourceType;
+        
+        // Input source (only one used based on sourceType)
+        public KeyCode Key;                     // For KeyCode inputs
+        public string AxisName;                 // For legacy axis inputs
+#if ENABLE_INPUT_SYSTEM
+        public UnityEngine.InputSystem.InputActionReference ActionRef; // For new input system actions
+#endif
+        
+        // Target
+        [Tooltip("Bizhawk control name e.g. \"D-Pad Up\" (Don't include \"P1 \" prefix!)")]
+        public string Control;
+        [Tooltip("Which controller this key is for (can be None)")]
+        public Controller Controller;
+        
+        // For sending analog input to bizhawk
+        [Tooltip("Whether this is an analog input on the bizhawk side")]
+        public bool IsAnalog;
+        [Tooltip("Minimum value for analog input (probably 0)")]
+        public int MinValue;
+        [Tooltip("Maximum value for analog input (probably 255)")]
+        public int MaxValue;
     }
 
     // Mappings from keyname to bizhawk console button name (can be many-to-many)
@@ -28,12 +53,14 @@ public class Controls {
     }
 
     // Map from a keyname to a list of (button name, controller) tuples
+    // TODO update
     public List<(string, Controller)> this[KeyCode key] {
         get {
             // Super inefficient TODO should probably store a Dictionary<string, List<(string, Controller)>> that gets updated when mappings changes
             List<(string, Controller)> result = new();
             foreach (var mapping in mappings) {
-                if (mapping.Key == key && mapping.Enabled) {
+                if (!mapping.Enabled) continue;
+                if (mapping.sourceType == InputSourceType.KeyCode && mapping.Key == key) {
                     result.Add((mapping.Control, mapping.Controller));
                 }
             }
@@ -42,8 +69,8 @@ public class Controls {
     }
     
     // Get all KeyCodes that are mapped in the controls
-    public HashSet<KeyCode> AllKeyCodes => mappings.Where(m => m.Enabled).Select(m => m.Key).ToHashSet();
-
+    public HashSet<KeyCode> AllKeyCodes => mappings.Where(m => m.Enabled && m.sourceType == InputSourceType.KeyCode).Select(m => m.Key).ToHashSet();
+    
     // Note: Slightly problematic because control labels are specific to the core rather than the system,
     // so e.g. Nymashock (PSX) uses "P1 △" where Octoshock (PSX) uses "P1 Triangle".
     // Bizhawk Emulation api doesn't seem to have a nice way to get the current core, so we can either
